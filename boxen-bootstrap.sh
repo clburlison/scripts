0c586bd9696ac50220385fe3e583d834b567db6a
Clayton Burlison <clburlison@gmail.com>
2014-05-09 14:01:20 -0500

    update bootstrap script. removed comments that are no longer needed thanks to boxen updates

--- boxen-bootstrap.sh
+++ boxen-bootstrap.sh
@@ -12,18 +12,14 @@ git clone https://github.com/clburlison/my-boxen.git /opt/boxen/repo
 cd /opt/boxen/repo
 
 
-# The beginning flag is because of an Apple bug with the new version of xcode. https://github.com/boxen/our-boxen/issues/528
-# This should have been fixed in https://github.com/boxen/our-boxen/commit/d254b360c9cec4b2d82e83f18218672bf5886a18
-#ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future ./script/boxen --no-fde
-
 # Do not require encryption (Useful for development):
 ./script/boxen --no-fde
 
 # add boxen to your shell config, at the end, eg. This is done with my dotfiles repo
 # echo '[ -f /opt/boxen/env.sh ] && source /opt/boxen/env.sh'>> ~/.bashrc
 
-
 echo "You need to setup your shell profile to work with boxen. For me I need to setup my dotfiles."
+echo " "
 echo "Setup dotfiles with the following command:"
 echo "cd src/mine/dotfiles; ./bootstrap.sh"
 echo "boxen --env"

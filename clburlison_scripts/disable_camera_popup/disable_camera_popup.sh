0d366b2af9998b7716431c9f479a93ce8695e474
Clayton Burlison <clburlison@gmail.com>
2015-12-02 09:55:59 -0600

    Updated for 10.11. GO AWAY PHOTOS.app I DON'T WANT YOU.

--- clburlison_scripts/disable_camera_popup/disable_camera_popup.sh
+++ clburlison_scripts/disable_camera_popup/disable_camera_popup.sh
@@ -4,9 +4,11 @@
 #
 # Updates:
 #   July 14, 2015    - Initial release 
-#   October 14, 2015 - Use -currentHost flag instead of $UUID 
+#   October 14, 2015 - Use -currentHost flag instead of $UUID
+#   December 2, 2015 - Updated for El Captain, now using a new preference
 #
 
 defaults -currentHost write com.apple.ImageCapture2 HotPlugActionPath ""
 defaults -currentHost write com.apple.ImageCapture2 LastHotPlugActionPath ""
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool YES
 exit 0
